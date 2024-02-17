0c202e8853bc2f857a17eb1d2b68abb546f58be5
ddunne6 <ddunne6@tcd.ie>
2024-02-17 16:07:01 +0000

    update experience text

--- src/dunnedev/experience.js
+++ src/dunnedev/experience.js
@@ -11,7 +11,7 @@ As part of my Master's degree, I wrote a dissertation on the topic of Machine Tr
 
 let myExperience = `
 During my time at university, I completed a Software Engineering internship at LexisNexis Risk Solutions.
-I now work at Amazon in the AWS CloudWatch Metrics department.
+I now work at Stripe in the EMEA Wallets department.
 `
 
 function DunnedevExperience() {
@@ -28,7 +28,7 @@ function DunnedevExperience() {
                     </p>
                 </Col>
                 <Col md>
-                    <DunnedevExperienceCell num="01/" jobTitle="Software Engineer" company="Stripe" start="February 2024" end="Present" startMonth={1} startYear={2024} endMonth={-1} endYear={-1}/>
+                    <DunnedevExperienceCell num="01/" jobTitle="Software Engineer" company="Stripe, Inc" start="February 2024" end="Present" startMonth={1} startYear={2024} endMonth={-1} endYear={-1}/>
                     <DunnedevExperienceCell num="02/" jobTitle="Software Development Engineer" company="Amazon Web Services" start="June 2022" end="February 2024" startMonth={5} startYear={2022} endMonth={1} endYear={2024}/>
                     <DunnedevExperienceCell num="03/" jobTitle="Software Engineer Intern" company="LexisNexis Risk Solutions" start="February 2021" end="August 2021" startMonth={1} startYear={2021} endMonth={7} endYear={2021}/> 
                     <DunnedevExperienceCell num="04/" jobTitle="Computer Engineer Student (B.A., M.A.I.)" company="Trinity College Dublin" start="September 2017" end="May 2022" startMonth={8} startYear={2017} endMonth={4} endYear={2022} />
